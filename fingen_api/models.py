f8f41872d1b026aaa3988a87bbe204d0311f8064
KamauDev-maker <oscarnjenga@gmail.com>
2023-12-15 15:02:34 -0500

    creating models for Quotation, Invoice, and LedgerAccount

--- fingen_api/models.py
+++ fingen_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Quotation(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    
+class Invoice(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    sent = models.BooleanField(default=False)
+    
+class LedgerAccount(models.Model):
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
