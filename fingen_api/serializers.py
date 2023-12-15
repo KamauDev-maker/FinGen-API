067e67caac8fcbea79d42417c44e879a4d73c3e4
KamauDev-maker <oscarnjenga@gmail.com>
2023-12-15 15:10:38 -0500

    creating serializers for my models

---
+++ fingen_api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Quotation, Invoice, LedgerAccount
+
+class QuotationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quotation
+        fields = '__all__'
+        
+class InvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+        
+class LedgerAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LedgerAccount
+        fields = '__all__'
