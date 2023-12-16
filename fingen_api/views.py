525039834f95397a17d1b5e29ebf16b897a69bf8
KamauDev-maker <oscarnjenga@gmail.com>
2023-12-16 04:58:52 -0500

    creating generic view for the application

--- fingen_api/views.py
+++ fingen_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Quotation, Invoice, LedgerAccount
+from .serializers import QuotationSerializer, InvoiceSerializer, LedgerAccountSerializer
 
-# Create your views here.
+
+class QuotationListCreateView(generics.ListCreateAPIView):
+    queryset = Quotation.objects.all()
+    serializer_class = QuotationSerializer
+    
+    def perform_create(self, serializer):
+        quantity - self.request.data.get('quantity', 1)
+        serializer.save(quantity=quantity)
+        
+class InvoiceListCreateView(generics.ListCreateAPIView):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+    
+class LegderAccountListCreateView(generics.ListCreateAPIView):
+    queryset = LegderAccount.objects.all()
+    serializer_class = LegderAccountSerializer
